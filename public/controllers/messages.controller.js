d801bd70dfe61c9447e728cbf05b05a228222415
riguel <asriguel@gmail.com>
2017-05-31 16:02:48 +0300

    1

---
+++ public/controllers/messages.controller.js
@@ -0,0 +1,65 @@
+app.controller('messagesController', function ($scope, $rootScope, $uibModal, $window, $http, vkService, toaster) {
+    this.dialogs = [];
+    this.messages = [];
+
+    // init vkService
+    vkService.setClientId(6033392);
+
+    let services = [vkService];
+
+    this.updateDialogs = () => {
+        services.forEach((service) => {
+            if (service.connected) {
+                service.getDialogs()
+                    .then(dialogs => {
+                        this.dialogs = [];
+                        this.dialogs = this.dialogs.concat(dialogs);
+                    })
+                    .catch(err => toaster.pop('error', err.service, err.message));
+            }
+        });
+    };
+    this.updateDialogs();
+
+    $rootScope.$on('updateDialogs', this.updateDialogs);
+
+    $rootScope.$on('scrollBottom', () => {
+        var scroller = document.getElementById("main");
+        scroller.scrollTop = scroller.scrollHeight;
+    });
+
+    this.textareaAction = () => {
+        $rootScope.currentDialog.sendMessage($scope.message);
+    };
+
+    this.addService = () => {
+        $uibModal.open({
+            templateUrl: 'assets/html/newServiceModal.html',
+            controller: function ($scope, $uibModalInstance, services) {
+                $scope.newService = '0';
+                $scope.services = services;
+                $scope.cancel = () => $uibModalInstance.close();
+                $scope.ok = () => {
+                    $scope.newService.auth();
+                    $uibModalInstance.close();
+                };
+            },
+            resolve: {
+                services: () => services
+            }
+        });
+    };
+
+    this.rerenderMessages = (dialog) => {
+        $rootScope.currentDialog = dialog;
+        this.messages = [];
+
+        dialog.getMessages()
+            .then(messages => {
+                this.messages = messages;
+            })
+            .catch(err => toaster.pop('error', err.service, err.message));
+    };
+
+    $rootScope.$on('rerenderMessages', () => this.rerenderMessages($rootScope.currentDialog));
+});
