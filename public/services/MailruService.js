a5422d0af4e17b8bd4d8df4182897e832e9310cd
Ilya Pinyagin <ivp4797@mail.ru>
2017-06-07 15:03:19 +0500

    big refactoring in Mail.ru service

--- public/services/MailruService.js
+++ public/services/MailruService.js
@@ -11,21 +11,29 @@ class MailruService extends BaseService {
 		
 		this.apiURL = "http://appsmail.ru/platform/api";
 		this.privateKey = "7c97f09acae3d5dede5542ffecec1f77";
-		this.pollTimeout = 30000;
 		
-		console.log(`Mail.ru service successfully set up`);
+		this.authURL = "https://connect.mail.ru/oauth/authorize";
+		this.authConfig = {
+			redirect_uri: "http://connect.mail.ru/oauth/success.html",
+			response_type: "token",
+			scope: "photos messages"
+		};
+		
+		this.pollTimeout = 5000;
+		
+		this.threadTimestamps = {};
+		
 		this.restoreSession();
     }
 	
 	restoreSession() {
-		console.log(`Attempting to restore previous authorized mail.ru session`);
 		const session_key = this.$cookies.get("mailru_token"), uid = this.$cookies.get("mailru_uid");
 		if (session_key && uid) {
-			console.log(`Mail.ru session key & uid restored from cookies: session_key=${session_key}, uid=${uid}`);
+			super.log(`session key & uid restored from cookies: session_key=${session_key}, uid=${uid}`);
             this.connect(session_key, uid);
         }
 		else {
-			console.log(`No previous mail.ru session restored`);
+			super.log(`No previous session restored`);
 		}
 	}
 	
@@ -34,157 +42,153 @@ class MailruService extends BaseService {
 	}
 	
 	makeSig(params) {
-		const paramString = Object.keys(params).sort().map(key => `${key}=${params[key]}`).join("");
-		const sigString = `${this.uid}${paramString}${this.privateKey}`;
+		const requestString = super.buildRequestString(params, { sort: true, joiner: "" });
+		const sigString = `${this.uid}${requestString}${this.privateKey}`;
 		return md5(sigString);
 	}
 	
 	callApiMethod(methodName, restParams) {
 		restParams = restParams || {};
-		const params = {
+		const params = Object.assign({}, restParams, {
 			app_id: this.clientId,
 			method: methodName,
-			session_key: this.token,
-			secure: 0,
-			format: "json"
-		};
-		Object.keys(restParams).forEach(key => params[key] = restParams[key]);
-		const sig = this.makeSig(params);
-		params.sig = sig;
-		const requestParams = Object.keys(params).map(key => `${key}=${params[key]}`).join("&");
-		const request = `${this.apiURL}?${requestParams}`;
-		console.log(`Calling mail.ru API method ${methodName}: ${request}`);
-		return this.$http.get(request);
-	}
-	
-	initUser() {
-		console.log(`Initializing mail.ru user`);
-		return this.callApiMethod("users.getInfo").then(response => {
-			if (response.data.error) {
-				console.error(`Failed to initialize mail.ru user: ${JSON.stringify(response.data.error)}`);
-				this.$cookies.remove('mailru_token');
-				this.$cookies.remove("mailru_uid");
-				throw { service: this.name, message: response.data.error.error_msg, showPopup: true };
-			}
-			const user = response.data[0];
-			console.log(`mail.ru user: ${JSON.stringify(user)}`);
-			this.$rootScope.mailru = {
-				id: user.uid,
-				full_name: `${user.first_name} ${user.last_name}`,
-				photo: user.has_pic ? user.pic : ""
-			};
-		}).catch(err => {
-			console.error("initUser failed:");
-			console.error(err);
-			this.toaster.pop("error", err.service, err.message);
+			session_key: this.token
 		});
+		params.sig = this.makeSig(params);
+		const url = super.buildRequestURL(this.apiURL, params);
+		super.log(`Calling API method ${methodName}: ${url}`);
+		return this.$http.get(url).then(
+			({ data }) => {
+				if (data.error) {
+					throw {
+						service: this.name,
+						message: `API method ${methodName} failed due to: ${data.error.error_msg}`,
+						showPopup: true
+					};
+				}
+				return data;
+			}
+		);
 	}
 	
-	poll() {
-		console.log(`Polling...`);
-		if (this.$rootScope.currentDialog && this.$rootScope.currentDialog.service === "mailru") {
-			console.log(`Current dialog belongs to mail.ru`);
-			this.$rootScope.$emit("reloadCurrentDialog");
-			this.$rootScope.$emit("scrollBottom");
-		}
-		this.$rootScope.$emit("reloadDialogList");
-	}
-	
-	setupPoller() {
-		setInterval(() => this.poll(), this.pollTimeout);
-		console.log("Poller initialized");
+	initPoller() {
+		setInterval(() => {
+			this.callApiMethod("messages.getThreadsList", { uid: this.uid }).then(
+				data => {
+					const dirtyThreads = data.filter(({ time, user: { uid } }) => {
+						return this.threadTimestamps[uid] && this.threadTimestamps[uid] == time;
+					});
+					const { currentDialog } = this.$rootScope;
+					const isCurrentThreadDirty =
+							currentDialog
+							&& currentDialog.service === "mailru"
+							&& dirtyThreads.find(({ user: { uid } }) => currentDialog.user_id == uid);
+					data.forEach(({ time, user: { uid } }) => {
+						this.threadTimestamps[uid] = time;
+					});
+					if (dirtyThreads.length > 0) {
+						this.$rootScope.$emit("reloadDialogList");
+					}
+					if (isCurrentThreadDirty) {
+						this.$rootScope.$emit("reloadCurrentDialog");
+					}
+				}
+			);
+		}, this.pollTimeout);
 	}
 	
 	connect(token, uid) {
 		this.token = token;
 		this.uid = uid;
         this.connected = true;
-		
-		console.log(`Connecting to mail.ru...`);
-		return this.initUser().then(() => this.setupPoller()).then(() => this.$rootScope.$emit("reloadDialogList"));
+		return this.callApiMethod("users.getInfo").then(
+			([ user ]) => {
+				this.$rootScope.mailru = {
+					id: user.uid,
+					full_name: `${user.first_name} ${user.last_name}`,
+					photo: user.pic
+				};
+				super.log(`Connect successful: ${JSON.stringify(this.$rootScope.mailru)}`);
+				this.$rootScope.$emit("reloadDialogList");
+				this.initPoller();
+			}
+		).catch(err => {
+			this.$cookies.remove("mailru_token");
+			this.$cookies.remove("mailru_uid");
+			throw err;
+		});
 	}
 	
 	auth() {
-		const authURL = "https://connect.mail.ru/oauth/authorize";
-		const redirectURI = "http://connect.mail.ru/oauth/success.html";
-		const responseType = "token";
-		const privileges = [ "photos", "messages" ];
-		const scope = privileges.join(" ");
-		const params = [
-			`client_id=${this.clientId}`,
-			`redirect_uri=${redirectURI}`,
-			`response_type=${responseType}`,
-			`scope=${scope}`
-		].join("&");
-		const authRequest = `${authURL}?${params}`;
-		
-		this.$window.open(authRequest, '_blank');
-        let authModal = this.$uibModal.open({
+		const requestURL = super.buildRequestURL(
+			this.authURL,
+			Object.assign({}, this.authConfig, { client_id: this.clientId })
+		);
+		this.$window.open(requestURL, '_blank');
+        const authModal = this.$uibModal.open({
             templateUrl: 'assets/html/mailruAuthModal.html',
             controller: function ($scope, $uibModalInstance) {
                 $scope.cancel = () => $uibModalInstance.close();
                 $scope.ok = () => $uibModalInstance.close({ token: $scope.token, uid: $scope.uid });
             }
         });
-		
         authModal.result
             .then(({ token, uid }) => {
                 this.$cookies.put('mailru_token', token);
 				this.$cookies.put(`mailru_uid`, uid);
-				console.log(`Authorization successful: token=${token}, uid=${uid}`);
+				super.log(`Authorization successful: token=${token}, uid=${uid}`);
                 return this.connect(token, uid);
             });
 	}
 	
-	getDialogMessages(thread) {
-		return this.callApiMethod("messages.getThread", { uid: thread.user.uid }).then(response => {
-			if (response.data.error) {
-				console.log(`Failed to load dialog messages: ${JSON.stringify(response.data.error)}`);
-				this.$cookies.remove("mailru_token");
-				this.$cookies.remove("mailru_uid");
-				throw { service: this.name, message: response.data.error.error_msg };
-			}
-			return response.data.map(message => {
-				console.log(`Processing message: ${JSON.stringify(message)}`);
-				const isMy = message.type === 0;
-				return {
-					text: message.message.map(msg => String(msg.content)).join(""),
-					date: new Date(message.time * 1000),
-					photo: isMy ? this.$rootScope.mailru.photo : thread.user.pic,
-					full_name: isMy ? this.$rootScope.mailru.full_name : `${thread.user.first_name} ${thread.user.last_name}`
-				};
-			}).reverse();
-		});
+	getMessage({ message, time, type }, { full_name, photo }) {
+		const isMy = type === 0;
+		return {
+			text: message.map(({ content }) => content).join(""),
+			date: new Date(time * 1000),
+			full_name: isMy ? this.$rootScope.mailru.full_name : full_name,
+			photo: isMy ? this.$rootScope.mailru.photo : photo
+		};
 	}
 	
-	sendDialogMessage(message, uid) {
-		console.log(`Sending message ${JSON.stringify(message)} to user ${uid}`);
-		return this.callApiMethod("messages.post", { uid, message });
+	getDialogMessages(dialog) {
+		return this.callApiMethod("messages.getThread", { uid: dialog.user_id }).then(
+			data => data.map(message => this.getMessage(message, dialog)).reverse()
+		);
+	}
+	
+	sendDialogMessage({ user_id }, message) {
+		return this.callApiMethod("messages.post", { uid: user_id, message });
+	}
+	
+	getDialog(thread, lastMessage) {
+		const dialog = {
+			service: "mailru",
+			text: lastMessage.message.map(part => String(part.content)).join(""),
+			unread: Boolean(thread.unread),
+			date: new Date(thread.time * 1000),
+			user_id: thread.user.uid,
+			full_name: `${thread.user.first_name} ${thread.user.last_name}`,
+			photo: thread.user.pic,
+			type: 1,
+			getMessages: () => this.getDialogMessages(dialog),
+			sendMessage: message => this.sendDialogMessage(dialog, message)
+		};
+		return dialog;
 	}
 	
 	getDialogs() {
-		return this.callApiMethod("messages.getThreadsList").then(response => {
-			if (response.data.error) {
-				console.log(`Failed to load dialogs: ${JSON.stringify(response.data.error)}`);
-				this.$cookies.remove("mailru_token");
-				this.$cookies.remove("mailru_uid");
-				throw { service: this.name, message: response.data.error.error_msg };
+		return this.callApiMethod("messages.getThreadsList").then(
+			data => {
+				return Promise.all(data.map(({ user: { uid } }) => {
+					return this.callApiMethod("messages.getThread", { uid, limit: 1 }).then(
+						([ message ]) => message
+					);
+				})).then(messages => {
+					return data.map((thread, i) => this.getDialog(thread, messages[i]));
+				});
 			}
-			return response.data.map(thread => {
-				console.log(`Processing thread: ${JSON.stringify(thread)}`);
-				return {
-					service: "mailru",
-					unread: Boolean(thread.unread),
-					date: new Date(thread.time * 1000),
-					user_id: thread.user.uid,
-					full_name: `${thread.user.first_name} ${thread.user.last_name}`,
-					photo: thread.user.pic,
-					getMessages: () => this.getDialogMessages(thread),
-					sendMessage: message => this.sendDialogMessage(message, thread.user.uid),
-					type: "1" //TODO conversations
-				};
-			});
-		});
+		);
 	}
 }
 
